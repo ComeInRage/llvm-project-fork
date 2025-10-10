f9e1d263a9b1ba77085baaf2bcc1164c6d28a57e
ComeInRage <montekek011@mail.ru>
2025-10-11 00:08:50 +0300

    Added start files

---
+++ llvm/include/llvm/Transforms/Utils/InversePlusMinus.h
@@ -0,0 +1,31 @@
+//===-  ------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+//
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TRANSFORMS_UTILS_INVERSE_PLUS_MINUS_H
+#define LLVM_TRANSFORMS_UTILS_INVERSE_PLUS_MINUS_H
+
+#include "llvm/IR/PassManager.h"
+#include "llvm/Support/Compiler.h"
+
+namespace llvm
+{
+
+    class Function;
+
+    struct InversePlusMinusPass
+    {
+      LLVM_ABI PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM);
+    };
+
+} // end namespace llvm
+
+#endif // LLVM_TRANSFORMS_UTILS_INVERSE_PLUS_MINUS_H
---
+++ llvm/lib/Transforms/Utils/InversePlusMinus.cpp
@@ -0,0 +1,8 @@
+#include "llvm/Transforms/Utils/InversePlusMinus.h"
+
+namespace llvm
+{
+    PreservedAnalyses InversePlusMinusPass::run(Function& F, FunctionAnalysisManager& AM)
+    {
+    }
+}
