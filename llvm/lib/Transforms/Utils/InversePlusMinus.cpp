a67ea09a1530f4bfb9460f16d470095fadd371ed
ComeInRage <montekek011@mail.ru>
2025-10-11 12:46:15 +0300

    Naive implementation

--- llvm/lib/Transforms/Utils/InversePlusMinus.cpp
+++ llvm/lib/Transforms/Utils/InversePlusMinus.cpp
@@ -1,8 +1,36 @@
 #include "llvm/Transforms/Utils/InversePlusMinus.h"
+#include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/Instructions.h"
 
-namespace llvm
-{
-    PreservedAnalyses InversePlusMinusPass::run(Function& F, FunctionAnalysisManager& AM)
-    {
+namespace llvm {
+PreservedAnalyses InversePlusMinusPass::run(Function &F,
+                                            FunctionAnalysisManager &AM) {
+  for (auto &&BB : F) {
+    for (auto &&instr : BB) {
+      if (auto binary = dyn_cast<BinaryOperator>(&instr)) {
+        bool isAddition = false;
+
+        switch (binary->getOpcode()) {
+        case BinaryOperator::BinaryOps::Add: {
+          isAddition = true;
+          [[fallthrough]];
+        }
+        case BinaryOperator::BinaryOps::Sub: {
+          auto newOpcode = isAddition ? BinaryOperator::BinaryOps::Sub
+                                      : BinaryOperator::BinaryOps::Add;
+          auto newBinary = BinaryOperator::Create(newOpcode, binary->getOperand(0),
+                                                  binary->getOperand(1), binary->getName());
+          newBinary->copyMetadata(*binary);
+          ReplaceInstWithInst(binary, newBinary);
+          break;
+        }
+        default:
+          break;
+        }
+      }
     }
+  }
 }
+} // namespace llvm
